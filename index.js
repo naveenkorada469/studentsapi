aa8143f46c46e40573362cb90f2d6636fb9ad508
naveenkorada469 <naveenkorada469@gmail.com>
2026-06-06 14:53:17 +0530

    change

--- index.js
+++ index.js
@@ -1,58 +1,39 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
- const dns=require("dns");
- dns.setServers(["8.8.8.8","1.1.1.1"]);
+const axios = require("axios");
+const productRouter = require("./routes/productRouter");
+const dns = require("dns");
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 require("dotenv").config();
 
-const authRoutes = require("./routes/authRoutes");
+const connectDb = require("./config/db");
+
+const userRouter = require("./routes/productRouter");
 
 const app = express();
 
-
-// MIDDLEWARE
-
 app.use(cors());
+
 app.use(express.json());
+app.use("/api/products", productRouter);
 
+// Database Connection
+connectDb();
 
-// HOME ROUTE
+// Routes
+app.use("/", userRouter);
 
+// Home Route
 app.get("/", (req, res) => {
-
-  res.send("API Running Successfully");
-
+    res.send("<h1>Backend Server Running</h1>");
 });
+// API Routes
+app.use("/api", userRouter);
 
 
-// AUTH ROUTES
-
-app.use("/api/auth", authRoutes);
-
-
-// MONGODB CONNECTION
-
-mongoose.connect(process.env.MONGO_URL)
-
-.then(() => {
-
-  console.log("MongoDB Connected ✅");
-
-  app.listen(process.env.PORT, () => {
-
-    console.log(
-      `Server Running On Port ${process.env.PORT}`
-    );
-
-  });
-
-})
-
-.catch((error) => {
-
-  console.log("MongoDB Connection Error ❌");
-
-  console.log(error);
-
+// Server
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
 });
